0c163c9f1f7be91dfcdd47407c20c840272c7cce
Adrian Carriger <mail@adrianandnicole.com>
2016-09-20 15:54:22 -0700

    feat: lambda can post data

--- src/lambda.js
+++ src/lambda.js
@@ -13,26 +13,55 @@ var config = require("./config.js").config;
 
 exports.handler = (event, context) => {
 
-  var queryParams = event.queryParams;
+  init(event.queryParams);
+  
+  // Run Firebase
+  auth()
+    .then(getData)
+    .then(sortData)
+    .then(postData)
+    .then(context.succeed);
 
-  // Initialize Firebase
-  firebase.initializeApp(config.firebase.config);
-  // Authenticate
-  var email = config.firebase.credentials.email;
-  var password = config.firebase.credentials.password;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(info => {
-      getData( (data) => context.succeed('data: ' + data) );
-    })
-    .catch( error => console.log(error) );
 };
 
-function getData(callback) {
-  var database = firebase.database();
+function init(params) {
+  // Initialize Firebase
+  firebase.initializeApp(config.firebase.config);
+    // Optional Firebase logging
+  if (params !== undefined && params.logging === true) {
+    firebase.database.enableLogging(true);
+  }
+}
 
-  var ref = database.ref('testing');
+function auth() {
+  return firebase.auth().signInWithEmailAndPassword(
+    config.firebase.credentials.email,
+    config.firebase.credentials.password);
+}
 
-  ref.on('value', function(snapshot) {
-    callback(snapshot.val());
+function postData(postInfo) {
+  return new Promise( (resolve, reject) => {
+    var testRef = firebase.database().ref(postInfo.ref);
+    testRef.transaction( () => {
+      return postInfo.data;
+    }, () => {
+      resolve();
+    });
   });
+}
+
+function getData() {
+  return new Promise( (resolve, reject) => {
+    var database = firebase.database();
+    var ref = database.ref('test/ref');
+    ref.once('value', (snapshot) => {
+      resolve(snapshot.val());
+    });
+  }); 
+}
+
+function sortData(data2) {
+  var ref = 'test/ref';
+  var data = 'test data-' + Date.now();
+  return {ref: ref, data: data};
 }
